e5f802a658795a8a2a12c2eb72d72eea947d9ace
barkatullah-khan <khanbarkatullah110@gmail.com>
2024-07-29 18:58:11 -0700

    new file

---
+++ objects2.js
@@ -0,0 +1,48 @@
+const tinderUser={}
+tinderUser.id="1234";
+tinderUser.name="Sammy";
+tinderUser.isLoggedIn=false;
+// console.log(tinderUser);
+// const user={
+//   email:"barkat@wazir",
+//   newUser:{
+//     UserFullName:{
+//       firstname:"barkat",
+//       lastname: "wazir",
+//     }
+//   }
+
+//   }
+// console.log(user.newUser.UserFullName);
+const obj1={1:"a", 2:"b"}
+const obj2={2:"a", 4:"b"}
+// const obj3={obj1,obj2}
+// console.log(obj3);
+// const obj3=Object.assign({},obj1,obj2);
+// console.log(obj3);
+// const obj3={...obj1,...obj2}
+// console.log(obj3);
+// const users=[
+//   {
+//     id:1,
+//     email:"barkat@wazir"
+//   },
+//   {
+//      id:1,
+//     email:"barkat@wazir"
+//   },
+// ]
+// users[1].email;
+//  console.log(tinderUser);
+//  console.log(Object.keys(tinderUser));
+//  console.log(Object.values(tinderUser));
+//  console.log(Object.(tinderUser));
+// const course={
+//   coursreName:"js in urdu",
+//   price:"123",
+//   courseInstructer:"hitesh"
+
+// }
+// const {courseInstructer:instructer}=course
+// // console.log(courseInstructer);
+// console.log(instructer);
