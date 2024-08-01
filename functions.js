d7021bccc4bf3e249532907dfb3b906977b9749c
barkatullah-khan <khanbarkatullah110@gmail.com>
2024-07-31 21:34:26 -0700

    function with object

---
+++ functions.js
@@ -0,0 +1,41 @@
+function myName(){
+ console.log("this is barkat");
+}
+// myName();
+// function addTwo(number1,number2){
+// console.log(number1+number2)
+// }
+// addTwo(2,4);
+
+// function addTwo(number1,number2){
+//   let result=number1+number2;
+//   return result;
+//   }
+//   console.log(addTwo(2,6));
+//  function loginUserMessage(username){
+//   return `${username} just login`;
+//  }
+//  console.log(loginUserMessage("barkat"));
+function calculatecartprice(val,...num1){
+  return num1;
+}
+// console.log(calculatecartprice(200,400,300))
+// object pass to a function
+const user={
+  username:"baratk",
+  price:199
+}
+function handleObject(anyobject){
+console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+// handleObject(user)
+//  handleObject({
+//   username:"sam",
+//   price:399
+//  })
+// Array with Object
+ const Array=[200,300,400]
+ function GetNewaaray(any){
+  return any
+ }
+//  console.log(GetNewaaray(Array));
